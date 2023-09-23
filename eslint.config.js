abeb203b4a68133eb0d6bed39aebff015f3f59c8
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-23 18:11:02 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,13 @@
-import { defineFlatConfig } from 'eslint-define-config'
 import antfu from '@antfu/eslint-config'
 
-export default defineFlatConfig([
-  ...antfu,
-])
+export default antfu(
+  {
+    vue: false,
+    ignores: [
+      // eslint ignore globs here
+    ],
+  },
+  {
+    // overrides
+  },
+)
