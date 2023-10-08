b55a38646929b003bb0f53a3f7ed9b1a28cce8bf
Anthony Fu <anthonyfu117@hotmail.com>
2023-10-08 22:09:03 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,15 @@
+// @ts-check
 import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
-    vue: false,
     ignores: [
       // eslint ignore globs here
     ],
   },
   {
-    // overrides
+    rules: {
+      // overrides
+    },
   },
 )
